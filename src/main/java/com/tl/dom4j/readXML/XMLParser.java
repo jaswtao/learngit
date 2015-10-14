ec45ca916815af89bdbcda2f0e47024889e1e092
terrytao <taoli51@gmail.com>
2015-10-14 15:22:54 +0800

    optimize the xml parser

--- src/main/java/com/tl/dom4j/readXML/XMLParser.java
+++ src/main/java/com/tl/dom4j/readXML/XMLParser.java
@@ -2,7 +2,10 @@ package com.tl.dom4j.readXML;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
@@ -17,15 +20,28 @@ public class XMLParser {
 								"<code>CSD000</code>"	+ 
 								"<msg>处理成功</msg>"		+ 
 								"<user_id>20140153</user_id>" + 
+								"<test><aaa>aaacontent</aaa><bbb><ccc>cccContents</ccc></bbb></test>" +
 							"</crs>";
 		Document document = parse(source);
 		Element root = document.getRootElement();
-		for (Iterator<Element> it = root.elementIterator(); it.hasNext(); ) {
-			Element el = it.next();
-			System.out.println();
+		/*for (Iterator<?> it = root.elementIterator(); it.hasNext(); ) {
+			Element el = (Element)it.next();
 			System.out.println(el.getName() + "--" + el.getStringValue());
-		}
+			List<?> childs = el.elements();
+			System.out.println(childs);
+			if (childs != null && childs.size() > 0) {
+				for (int i = 0; i < childs.size(); i++) {
+					Element child = (Element) childs.get(i);
+					System.out.println(child.getName() + "==" + child.getStringValue());	
+				}
+			}
+		}*/
+		Map<String, String> contents = new HashMap<String, String>();
+		parseToMap(root, contents);
+		System.out.println(index);
+		System.out.println(contents);
 	}
+	private static int index = 0;
 
 	public static Document parse(String xmlSource) {
 		SAXReader reader = new SAXReader();
@@ -37,5 +53,19 @@ public class XMLParser {
 			e.printStackTrace();
 		}
 		return document;
+	}
+	
+	public static void parseToMap(Element root, Map<String, String> container) {
+		index++;
+		List<?> childs = root.elements();
+		if (childs == null || childs.size() == 0) {
+			container.put(root.getName(), root.getStringValue());
+		} else {
+			for (int i = 0; i < childs.size(); i++) {
+				Element child = (Element)childs.get(i);
+				parseToMap(child, container);
+			}
+
+		}
 	}
 }
