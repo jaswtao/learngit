92c136845c0849e8ba9b7c2137b6e82c1ac05a87
terrytao <taoli51@gmail.com>
2016-04-27 15:22:07 +0800

    add random test

---
+++ src/main/java/com/tl/jdktest/numberic/TestRandom.java
@@ -0,0 +1,10 @@
+package com.tl.jdktest.numberic;
+
+public class TestRandom {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(Math.random() * 5);
+	}
+
+}
