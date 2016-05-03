09ae4bf6213bbaabaf98a4f3dd0557fbd7f2669c
terrytao <taoli51@gmail.com>
2016-05-03 20:26:58 +0800

    adding test

--- src/main/java/com/tl/date/datemain/DateTest.java
+++ src/main/java/com/tl/date/datemain/DateTest.java
@@ -10,6 +10,14 @@ public class DateTest {
 		Date currDate = new Date();
 		System.out.println(currDate);
 		System.out.println(DateUtil.getFirstAuditTime(currDate));
+		
+		System.out.println(System.currentTimeMillis());
+		System.out.println(new Date().getTime());
+		
+		System.out.println(DateUtil.dateToString(DateUtil.toStartOfDay(currDate)));
+		System.out.println(DateUtil.dateToString(DateUtil.toEndOfDay(currDate)));
+		System.out.println(DateUtil.toStartOfDay(currDate).getTime());
+		System.out.println(DateUtil.toEndOfDay(currDate).getTime());
 	}
 
 }
--- src/main/java/com/tl/date/util/DateUtil.java
+++ src/main/java/com/tl/date/util/DateUtil.java
@@ -1,10 +1,14 @@
 package com.tl.date.util;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Random;
 
 public class DateUtil {
+	
+	public final static DateFormat YYYY_MM_DD_MM_HH_SS = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
 	/**
 	 * get the time between 10 and 16 o'clock on tomorrow
@@ -28,5 +32,44 @@ public class DateUtil {
 		hourAndMinutes[0] = 10 + rand.nextInt(7); // hour
 		hourAndMinutes[1] = rand.nextInt(60);
 		return hourAndMinutes;
+	}
+	
+	/**
+	 * 获取给定日期的开始时间对象，即当天的00点00分00秒
+	 * @param date
+	 * @return
+	 */
+	public static Date toStartOfDay(Date date) {
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(date);
+		int year = calendar.get(Calendar.YEAR);
+		int month = calendar.get(Calendar.MONDAY);
+		int day = calendar.get(Calendar.DAY_OF_MONTH);
+		calendar.set(year, month, day, 0, 0, 0);
+		return calendar.getTime();
+	}
+	
+	/**
+	 * 获取给定日期的最后时间对象，即当天的23点59分59秒
+	 * @param date
+	 * @return
+	 */
+	public static Date toEndOfDay(Date date) {
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(date);
+		int year = calendar.get(Calendar.YEAR);
+		int month = calendar.get(Calendar.MONDAY);
+		int day = calendar.get(Calendar.DAY_OF_MONTH);
+		calendar.set(year, month, day, 23, 59, 59);
+		return calendar.getTime();
+	}
+	
+	/**
+	 * 时间转换为yyyy-MM-dd HH:mm:ss格式的字符串
+	 * @param date
+	 * @return
+	 */
+	public static String dateToString(Date date){
+		return YYYY_MM_DD_MM_HH_SS.format(date);
 	}
 }
